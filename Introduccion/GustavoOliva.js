45efa9b4412c490267d0c5cccb187be498bebd6f
GustavoOliva <gutyoc@hotmail.com>
2018-02-11 20:54:30 -0600

    Tarea

---
+++ Introduccion/GustavoOliva.js
@@ -0,0 +1,30 @@
+mes = 10;
+
+switch (mes){
+	case 1:
+	case 2:
+	case 12:
+	alert("Es invierno");
+	break;
+
+	case 3:
+	case 4:
+	case 5:
+	alert("Es primavera");
+	break;
+
+	case 6:
+	case 7:
+	case 8:
+	alert("Es verano");
+	break;
+
+	case 9:
+	case 10:
+	case 11:
+	alert("Es otono");
+
+	default: 
+	alert("Gustavo Adolfo Oliva Campos 18003951");
+	break;
+}
