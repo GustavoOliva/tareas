60f8628710f4a2087b3e912247554e24c3c9fcf4
GustavoOliva <gutyoc@hotmail.com>
2018-02-11 23:14:52 -0600

    TAREA UNIDAD 3 PUNTOS EXTRAS 1 Y 2

--- Introduccion/GustavoOliva.js
+++ Introduccion/GustavoOliva.js
@@ -1,3 +1,80 @@
+
+//PUNTOS EXTRA 1
+var nombre = prompt ("Ingrese su nombre:");
+
+console.log("Bienvenido " + nombre);
+
+document.write("Bienvenido " + nombre);
+
+
+
+//PUNTOS EXTRA 2
+/* if nos sirve si queremos comprobar que un valor es verdadero, por ejemplo
+yo vivo en la casa 2 */
+
+
+var casa = 2;
+
+if(casa == 2){
+	document.write("Casa de Gustavo Oliva");
+}
+
+/*if else nos serviria para verificar si el valor asignado es o no es el de mi casa*/
+
+if(casa == 2){
+	document.write("Casas de Gustavo Oliva");
+}else{
+	document.write("Esta no es la casa de Gustavo Oliva");
+}
+
+
+/* if else nos serviria en el caso que quisieramos identificar a mas de un vecino*/
+/*
+  CASA 2 = GUSTAVO OLIVA
+  CASA 3 = SOFY
+  */
+if(casa == 2){
+	document.write("Casa de Gustavo Oliva");
+}else if(casa==3){
+	document.write("Casa de Sofy");
+}else{
+	document.write("No pertenece a este sector")
+}
+
+
+/*Switch case nos sirve cuando tenemos bastantes opciones por elegir*/
+/* Switch case nos serviria para averiguar en donde vive cualquier vecino de la colonia*/
+/*CASA 1 = BIBI
+  CASA 2 = GUSTAVO OLIVA
+  CASA 3 = SOFY
+  CASA 4 = ERICK*/
+
+  var numeroCasa = prompt("Ingrese el numero de casa:");
+  var n = parseInt(numeroCasa);
+
+  switch (n){
+  	case 1:
+  	alert("En la casa 1 vive Bibi");
+  	break;
+  	case 2:
+  	alert("En la casa 2 vive Gustav Oliva");
+  	break;
+  	case 3:
+  	alert("En la casa 3 vive Sofy");
+  	break;
+  	case 3:
+  	alert("En la casa 4 vive Erick");
+  	break;
+  	default:
+  	alert("No vive en la colonia");
+  	break;
+  }
+
+
+
+//ACTIVIDAD 3
+
+
 mes = 10;
 
 switch (mes){
@@ -28,3 +105,8 @@ switch (mes){
 	alert("Gustavo Adolfo Oliva Campos 18003951");
 	break;
 }
+
+
+
+
+
